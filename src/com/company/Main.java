63a4bbb8f8efaf4a4363bd566b755b31527bc7cf
Ryo Okuma <e195702@ie.u-ryukyu.ac.jp>
2019-12-08 13:21:04 +0900

    Step2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,8 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
+        try {
+            String str = null;
+            System.out.println(str.length());
+        }catch (NullPointerException e){
+            System.out.println("NullPointerExceptionが発生しました。");
+            System.out.println(e.getMessage());
+        }
 	// write your code here
     }
 }
