4e8e56bf2133085833f400064f03ac595193f4fb
Ryo Okuma <e195702@ie.u-ryukyu.ac.jp>
2019-12-08 12:40:48 +0900

    初期データ

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String str = null;
+        System.out.println(str.length());
+	// write your code here
+    }
+}
