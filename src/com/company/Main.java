007eb2b9182f155d13ff4b13baecbf2ee8f711e1
Ryo Okuma <e195702@ie.u-ryukyu.ac.jp>
2019-12-09 15:15:17 +0900

    Step3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        try {
-            String str = null;
-            System.out.println(str.length());
-        }catch (NullPointerException e){
-            System.out.println("NullPointerExceptionが発生しました。");
-            System.out.println(e.getMessage());
-        }
-	// write your code here
+        String str = "百二十三";
+        System.out.println(Integer.parseInt(str));
+
     }
 }
